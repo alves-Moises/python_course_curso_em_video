8ae2e89019549ca1ce80cdbcc09f926c68b58637
alves-Moises <moisesingo_ra@hotmail.com>
2021-02-24 01:53:38 -0300

    new: aulas

---
+++ aula08_a.py
@@ -0,0 +1,8 @@
+from math import sqrt, factorial, floor
+num = float(input('Digite um número: '))
+sqr = floor(sqrt(num))
+
+print(f'{factorial(sqr)}')
+
+
+
---
+++ aula08_b.py
@@ -0,0 +1,3 @@
+import emoji
+print(emoji.emojize('Hello fuckin world! :sunglasses:'))
+print(emoji.emojize())
